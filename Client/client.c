4ecd22cd25103c0911d8ff53d30d50ff439f384c
H2V <h2v9696@gmail.com>
2017-11-14 22:03:23 +0700

    Them makefile cho server, them server client

--- Client/client.c
+++ Client/client.c
@@ -1,2 +1,58 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 
+#define MAXLINE 4096 /*max text line length*/
+#define SERV_PORT 3000 /*port*/
+
+int main(int argc, char **argv) 
+{
+  int sockfd;
+  struct sockaddr_in servaddr;
+  char sendline[MAXLINE], recvline[MAXLINE];
+  
+  // Co the sua thanh fix cung server
+  if (argc !=2) {
+    perror("Usage: TCPClient <IP address of the server"); 
+    exit(1);
+  }
+  
+  // Create a socket for the client
+  if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+    perror("Problem in creating the socket");
+    exit(2);
+  }
+  
+  // Tao socket
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr= inet_addr(argv[1]);
+  servaddr.sin_port =  htons(SERV_PORT);
+	
+  // Ket noi den server 
+  if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
+    perror("Problem in connecting to the server");
+    exit(3);
+  }
+  // Gui message, them header vao day
+  while (fgets(sendline, MAXLINE, stdin) != NULL) {
+    // Loai bo Enter
+    sendline[strlen(sendline) - 1] = '\0';
+    send(sockfd, sendline, strlen(sendline) + 1, 0);
+
+    if (recv(sockfd, recvline, MAXLINE,0) == 0){
+      perror("The server terminated prematurely"); 
+      exit(4);
+    }
+    // Thay cai nay bang ten con ChatBot
+    printf("%s", "Tao la ChatBot: ");
+    fputs(recvline, stdout);
+    printf("\n");
+
+  }
+  
+  exit(0);
+}
--- Server/server.c
+++ Server/server.c
@@ -1,1 +1,67 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define MAXLINE 4096 // max text line length
+#define SERV_PORT 3000 // port
+#define LISTENQ 16 // maximum number of client connections
+
+int main ()
+{
+  int listenfd, connfd, n;
+  pid_t childpid;
+  socklen_t clilen;
+  char buff[MAXLINE];
+  struct sockaddr_in cliaddr, servaddr;
+  
+  //Create a socket for the soclet
+  //If sockfd<0 there was an error in the creation of the socket
+  if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+    perror("Problem in creating the socket");
+exit(2);
+  }
+  
+  //preparation of the socket address
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(SERV_PORT);
+ 
+  //bind the socket
+  bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+ 
+  //listen to the socket by creating a connection queue, then wait for clients
+  listen (listenfd, LISTENQ);
+
+  // printf("%s\n","Server running...waiting for connections.");
+ 
+  for ( ; ; ) {
+    clilen = sizeof(cliaddr);
+    // accept a connection
+    connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
+    
+    // printf("%s\n","Received request...");
+    // Neu la 0 thi la da fork sang process con
+    if ((childpid = fork()) == 0) {
+      //printf ("%s\n","Child created for dealing with client requests");
+      // close listening socket of child
+      close (listenfd);
+      while ((n = recv(connfd, buff, MAXLINE, 0)) > 0)  {
+	// Nhan buff tu client va gui answer o day
+	printf("%s","String received from and resent to the client:");
+	puts(buff);
+	send(connfd, buff, n, 0);
+      }
+      
+      if (n < 0)
+	printf("%s\n", "Read error");
+      // Handle close client socket, neu tat thi close
+      close(connfd);
+      exit(0);
+    }
+    // Dong socket cua server chinh
+    close(connfd);
+  }
+}
