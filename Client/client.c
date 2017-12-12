fee1cdbe62899c97d88071fbc658503983a3056d
H2V <h2v9696@gmail.com>
2017-12-12 22:52:15 +0700

    fix final 2

--- Client/client.c
+++ Client/client.c
@@ -69,7 +69,7 @@ void on_tey_mess_activate()
 
     while (remaining_bytes > 0) {
         int res = recv(sockfd , &receive_buffer[received_bytes] , remaining_bytes, 0);
-        if (res = 0) {
+        if (res == 0) {
             perror("The server terminated prematurely"); 
       	    exit(4);
         }
